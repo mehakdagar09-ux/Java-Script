34a28a5b0cbdfda58e99eb2afea706c34c0ec6e8
Mehak Dagar <mehakdagar09@gmail.com>
2026-01-28 12:56:55 +0530

    Js

---
+++ JS-Daily-Work/js_day-7/script.js
@@ -0,0 +1,42 @@
+
+// let user={
+//     fullname:"mehak dagar",
+//     address: {
+//        city: "jharsaintly village",
+//        State:"haryana",
+//     },
+//     mobileno:1234567890,
+//     favcolor:["white","red","pink"],
+//     demo:function(){
+//         return "demo function"
+//     }
+
+// }
+// console.log(user.fullname,user.mobileno,user.favcolor[1], user.address.city, user.demo() );
+
+// console.log("my name is "+user.fullname+". my fav color is"+user.favcolor[0]);
+
+// //string literal
+// console.log(`my name is ${user.fullname}. my fav color is ${user.favcolor[0]}`);
+
+// // object methods
+
+// //object.keys(variable name)
+
+// console.log(Object.keys(user));
+
+// console.log(Object.entries(user));
+
+
+// const car = {
+//     make: "Mahindra",
+//     model: "Thar"
+// };
+// //
+// //object.freeze(car); // we cant add a new key and value pair and we cant change or update the existing value
+
+// Object.seal(car)//we vant add a new key and value pair but we can change and update the existung value
+
+// car.model="xuv700"
+// car.color = "zblack";
+// console.log(car);
